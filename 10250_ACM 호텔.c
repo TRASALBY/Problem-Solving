ca3c876e219a0577de6d1650de4a3cb6bf6f54c5
TRASALBY <ghtjd8264@naver.com>
2021-02-12 16:08:57 +0900

    10250_ACM 호텔 해결
    방 번호는 YXX 나 YYXX 형태인데 여기서 Y 나 YY 는 층 수를 나타내고 XX 는 엘리베이터에서부터 세었을 때의 번호를 나타낸다. 즉, 그림 1 에서 빗금으로 표시한 방은 305 호가 된다.
    
    손님은 엘리베이터를 타고 이동하는 거리는 신경 쓰지 않는다. 다만 걷는 거리가 같을 때에는 아래층의 방을 더 선호한다. 예를 들면 102 호 방보다는 301 호 방을 더 선호하는데, 102 호는 거리 2 만큼 걸어야 하지만 301 호는 거리 1 만큼만 걸으면 되기 때문이다. 같은 이유로 102 호보다 2101 호를 더 선호한다.

--- 10250_ACM 호텔.c
+++ 10250_ACM 호텔.c
@@ -1,5 +1,19 @@
 #include    <stdio.h>
 
 int main(){
-    
+    int  w, h, n, t;
+    int x, y;
+
+    scanf("%d", &t);
+
+    for(int i = 0; i < t; i++){
+        scanf("%d %d %d", &h, &w, &n);
+        y = n % h;
+        x = n / h + 1;
+        if(y == 0){
+          y = h;
+          x = x-1;
+        }
+        printf("%d\n", y*100 + x);
+    }
 }
