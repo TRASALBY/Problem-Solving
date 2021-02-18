3fc396619fc438f01179ff28ebc1099d7b0ec4ab
TRASALBY <ghtjd8264@naver.com>
2021-02-18 20:17:42 +0900

    2292_벌집
    위의 그림과 같이 육각형으로 이루어진 벌집이 있다. 그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에 돌아가면서 1씩 증가하는 번호를 주소로 매길 수 있다. 숫자 N이 주어졌을 때, 벌집의 중앙 1에서 N번 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나가는지(시작과 끝을 포함하여)를 계산하는 프로그램을 작성하시오. 예를 들면, 13까지는 3개, 58까지는 5개를 지난다.
    https://www.acmicpc.net/JudgeOnline/upload/201009/3(2).png

---
+++ 2292_벌집.c
@@ -0,0 +1,26 @@
+#include    <stdio.h>
+
+int main(){
+    int n;
+    int count;
+    scanf("%d", &n);
+
+    count = 1;
+
+    if(n != 1){
+        if(count > n){
+            printf("%d", i);
+        }
+        for(int i = 0; ;i++){
+            count += i * 6;
+        }
+    }
+
+
+
+    //2~7       2
+    //8~19      3
+    //20~37     4
+    //38~61     5
+    
+}
