89f69ba9cd0a75230949440c25b0043f0dce847a
TRASALBY <ghtjd8264@naver.com>
2021-02-27 17:51:43 +0900

    11050_이항계수1
    자연수 N 과 정수 K 가 주어졌을 때 이항 계수
    를 구하는 프로그램을 작성하시오.
    
    이항계수는 N! / ((K!) * (N-K)!)으로 계산된다.

---
+++ 11050_이항계수1.c
@@ -0,0 +1,20 @@
+#include    <stdio.h>
+
+int fact(int x);
+
+int main(){
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+
+    printf("%d", fact(n)/(fact(k) * fact(n-k)));
+
+}
+
+int fact(int x){
+    int result = 1;
+    for(int i = 1 ; i <= x;i++){
+        result *= i;
+    }
+    return result;
+}
