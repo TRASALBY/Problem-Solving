00e12a91aac826cc8d5c08aa4a3e86d715115a57
TRASALBY <ghtjd8264@naver.com>
2022-01-29 22:55:30 +0900

    n-queen 해결
    pypy3로 해서 시간초과 해결

--- BOJ/9663_N-Queen.py
+++ BOJ/9663_N-Queen.py
@@ -1,4 +1,27 @@
-n = int(input())
+def solve(queen,n,y):
+    count = 0
+    
+    if n == y:
+        return 1
+    for col in range(n):
+        queen[y] = col
 
-graph = [[0 for _ in range(n)] for _ in range(n)] 
-print()
+        # for-else구문
+        for x in range(y):
+            # 세로로 겹치면 안됨
+            if queen[x] == queen[y]: 
+                break
+                
+            # 대각선으로 겹치면 안됨
+            if abs(queen[x]-queen[y]) == (y-x):
+                break
+        else:
+            count += solve(queen, n, y+1)
+
+    return count
+
+n = int(input())
+queen = [0]*n
+
+
+print(solve(queen,n,0))
