2bdbcf08b775cbc88ceaa00c203a227c233b1152
TRASALBY <ghtjd8264@naver.com>
2022-02-20 23:13:15 +0900

    단어정렬 수정
    입력을 set으로 받아 시작부터 중복을 제거 후 정렬

--- BOJ/1181_단어정렬.py
+++ BOJ/1181_단어정렬.py
@@ -1,17 +1,14 @@
 n = int(input())
-temp = []
+temp = set([])
 for i in range(n):
     word = input()
-    temp.append([word,len(word)])
+    temp.add(word)
+
+temp = list(temp)
+for i in range(len(temp)):
+    temp[i] = [temp[i],len(temp[i])]
 
 temp.sort(key = lambda x : (x[1], x[0]))
-for i in range(n):
-    temp[i] = temp[i][0]
-for i in range(n):
-    if i >0:
-        if temp[i-1] == temp[i]:
-            continue
-        else:
-            print(temp[i])
-    else:
-        print(temp[i])
+
+for i in range(len(temp)):
+    print(temp[i][0])
