30d43a84f7e2ed2fcfaa25eabad053a4d06c7105
TRASALBY <ghtjd8264@naver.com>
2022-02-28 18:48:34 +0900

    가장 큰수 해결
    각 숫자의 뒤에 반복되는 0을 붙이는 것으론 3과 30을 비교할 수 없다. (둘다 30000이 되어 버린다. 303과 330중에선 330이 되어야 한다.)
    각 숫자가 반복되어 증가하는 형식으로 변경하여 12와 121을 비교 할 수 있게 하였다. (0만 붙일경우 12000과 121000이 비교되어 12112이지만 반복되는 숫자로 하면 12121과 12112을 비교하여 12121이 된다.)
    변형된 숫자들을 크기를 비교하여 순서를 정렬하고 원래의 리스트에서 각 인덱스를 참조하여 문자열로 합친다.
    모든 숫자가 0일경우 문자열에서 0000의 형식으로 출력되므로 int로 감싼후 str로 변형하여 리턴한다.

--- Programers/가장큰수.py
+++ Programers/가장큰수.py
@@ -2,22 +2,26 @@ def solution(numbers):
     answer = ''
     temp_number = []
     ind = 0
-    for number in numbers:        
-        cnt = 0
-        
+
+    for number in numbers:
+        number = str(number)
         if number!= 0:
-            while number < 10000:
-                number *=  10
-                cnt+=1
-            temp_number.append([number,ind,cnt])
-            ind += 1
-        
-    temp_number.sort(key = lambda x: (x[0],x[2]))
+            i = 0
+            while  int(number) < 10000:
+                if number =='0':
+                    break
+                number += number[i]
+                i += 1
+                if i >= len(number):
+                    i =0
+            number = int(number)
+        temp_number.append([number,ind])
+        ind += 1
+
+    temp_number.sort(key = lambda x: (x[0]))
     temp_number.reverse()
     temp_answer = []
     for i in range(len(numbers)):   
         answer += str(numbers[temp_number[i][1]])
-                      
-    return answer
 
-print(solution([12, 121]))
+    return str(int(answer))
