9a5465293e5525d7ae2014d1227f2bfb8dc5e619
TRASALBY <ghtjd8264@naver.com>
2022-03-21 16:16:17 +0900

    14888_연산자 끼워넣기
    이유없는 런타임 에러시에는 잘못누른 자동완성으로 불필요한 import가 없는지 확인해 봐야 한다.

--- BOJ/14888_연산자 끼워넣기.py
+++ BOJ/14888_연산자 끼워넣기.py
@@ -1,4 +1,3 @@
-from multiprocessing.connection import answer_challenge
 import sys
 
 input = sys.stdin.readline
@@ -7,34 +6,30 @@ n = int(input())
 a = list(map(int, input().split()))
 add, sub, mul, div = map(int, input().split())
 
-max_val = -1e9
-min_val = 1e9
-count = 0
-def dfs(n,num,add,sub, mul, div,count):
+max_val = -float("inf") #최대 최소값 +-무한으로 지정
+min_val = float("inf")
+
+
+def dfs(n,num,add,sub, mul, div,count): #재귀적으로 모든 경로를 탐색하면서
     global max_val
     global min_val
 
-
-    if count == n -1:
+    count+=1
+    if count == n :              #모든 연산을 다 했을때 값을 최대 최소와 비교하여 저장
         max_val = max(max_val, num)
         min_val = min(min_val, num)
         return
-    count+=1
-    if add:
+    
+    if add:                     #각 연산 값이 0이 아닐때 재귀적으로 깊이탐색을 진행하며 모든 경우 연산 진행
         dfs(n,num + a[count],add-1,sub,mul,div,count)
     if sub:
         dfs(n,num - a[count],add,sub-1,mul,div,count)
     if mul:
         dfs(n,num * a[count],add,sub,mul-1,div,count)
-    if div:
-        if num < 0:
-            num = -num
-            dfs(n,-(num // a[count]),add,sub,mul,div-1,count)
-        else:
-            dfs(n,num // a[count],add,sub,mul,div-1,count)
+    if div: 
+        dfs(n,int(num / a[count]),add,sub,mul,div-1,count)
 
-    return
 dfs(n,a[0],add,sub, mul, div,0)
 
-print(max_val)
+print(max_val)                 #결과 출력
 print(min_val)
