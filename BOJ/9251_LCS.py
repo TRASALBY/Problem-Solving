ab21db7d2092aa1757cf70f5e29edda73b4070be
TRASALBY <ghtjd8264@naver.com>
2022-04-07 23:15:56 +0900

    9251_LCS
    쪼개서 dp로 생각하는 방식 ACAYKP / CAPCAK라면
    두번째 문자를 앞에서부터 하나씩 끊어서 표를 그려가며 해결해야 한다.

--- BOJ/9251_LCS.py
+++ BOJ/9251_LCS.py
@@ -8,28 +8,11 @@ arr2 = input().strip()
 len1 = len(arr1)
 len2 = len(arr2)
 
-dp = [[0 for _ in range(len1)] for _ in range(len2)]
-check_point = False
-for j in range(0,len1):
-    if check_point == True:
-        dp[0][j] = dp[0][j-1]
-        continue
-    if j == 0:
-        if arr2[0] == arr1[j]:
-            dp[0][j] = 1
-            check_point = True
-        else:
-            dp[0][j] = 0
-        continue
-    if arr2[0] == arr1[j]:
-        dp[0][j] = dp[0][j-1] + 1
-        check_point = True
-    else:
-        dp[0][j] = dp[0][j-1]
-for i in range(1,len2):
-    for j in range(0,len1):
-        if arr2[i] == arr1[j]:
-            dp[i][j] = dp[i][j-1] + 1
+dp = [[0 for _ in range(len1+1)] for _ in range(len2+1)]
+for i in range(1,len2+1):
+    for j in range(1,len1+1):
+        if arr2[i-1] == arr1[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i][j-1],dp[i-1][j])
 
