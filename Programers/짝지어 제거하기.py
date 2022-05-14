a7cabe8203a0aa5fdfff7d00c6ca2d9180deef85
TRASALBY <ghtjd8264@naver.com>
2022-05-14 18:20:36 +0900

    짝지어 제거
    스택을 사용해서 해결

--- Programers/짝지어 제거하기.py
+++ Programers/짝지어 제거하기.py
@@ -2,18 +2,19 @@ def solution(s):
     if len(s) % 2 == 1:
         return 0
     
-    while len(s) > 1:
-        start = -1
-        end = -1
-        for i in range(len(s)-1):                
-            if start == -1 and s[i] == s[i+1]:
-                start = i
-                end = i+2
-                break
-        if start == end:
-            return 0
-        s = s[0:start] + s[end:]
-        if len(s) == 2 and s[0] == s[1]:
-            return 1
+    s = list(s)
+    stack = []
+    
+    while s:
+        stack.append(s.pop())
+        if len(stack) >= 2:
+            if stack[-1] == stack[-2]:
+                stack.pop()
+                stack.pop()
+    
+    if stack:
+        return 0
+    else:
+        return 1
 
     return 0
