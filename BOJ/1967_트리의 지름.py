fddad621f01f0fdfc11db97cd530b0841b018e44
TRASALBY <ghtjd8264@naver.com>
2022-06-30 02:18:01 +0900

    0630 백준 골드4
    해법 자체는 BFS를 통한 노드사이의 길이 구하는 문제이다
    메모리 문제로 인해 그래프를 이중 리스트로 구현하지 못하게 되어있었고 모든 리프노드에 대해 거리를 탐색하면 시간초과가 발생한다.
    임의의 한 노드로 부터 가장 거리가 먼 노드를 구한다면 해당 노드는 그래프의 한쪽 끝이다.
    구해진 한쪽 끝으로 부터 가장 거리가 먼 노드까지의 거리를 구한다면 해당 그래프의 지름을 구할 수 있다.

--- BOJ/1967_트리의 지름.py
+++ BOJ/1967_트리의 지름.py
@@ -2,71 +2,46 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-INF = int(1e9)
 
-# 노드의 개수(n)과 간선의 개수(m) 입력
 n = int(input())
 
-# 2차원 리스트 (그래프 표현) 만들고, 무한대로 초기화
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
+graph = [[]for _ in range(n + 1)]
 
-# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
-        if a == b:
-            graph[a][b] = 0
-
-# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
-for _ in range(n-1):
-    # A -> B로 가는 비용을 C라고 설정
-    a, b, c = map(int, input().split())
-    graph[a][b] = c
-    graph[b][a] = c
+if n == 1:
+    print(0)
+else:
+    for _ in range(n-1):
+        a, b, c = map(int, input().split())
+        graph[a].append([b, c])
+        graph[b].append([a, c])
 
 
-leaf = []
-for a in range(1, n + 1):
-    if graph[a].count(INF) == len(graph[a])-2:
-        leaf.append(a)
-
-max_len = 0
-for start in leaf:
-    q = deque([start])
-    visited = [0] * (n+1)
-    dist = [0] * (n+1)
-
-    while(q):
-        v = q.popleft()
-
-        for i in range(len(graph[v])):
-            if graph[v][i] != INF and visited[i] == 0:
-                visited[i] = 1
-                q.append(i)
-                dist[i] += dist[v] + graph[v][i]
-
-    for d in dist:
-        if d > max_len:
-            max_len = d
-
-
-
-
-print(max_len)
-
-
-
-'''
-# 점화식에 따라 플로이드 워셜 알고리즘을 수행
-for k in range(1, n + 1):
+    leaf = []
     for a in range(1, n + 1):
-        for b in range(1, n + 1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+        if len(graph[a]) == 1:
+            leaf.append(a)
 
-max_len = 0
-for a in range(1, n + 1):
-        for b in range(1, n + 1):
-            if graph[a][b] > max_len:
-                max_len = graph[a][b]
+    max_len = 0
+    start = leaf[0]
+    for i in range(2):
+        q = deque([start])
+        visited = [0] * (n+1)
+        dist = [0] * (n+1)
+
+        visited[start] = 1
+        while(q):
+            v = q.popleft()
+
+            for i in graph[v]:
+                if visited[i[0]] == 0:
+                    visited[i[0]] = 1
+                    q.append(i[0])
+                    dist[i[0]] += dist[v] + i[1]
+        now_max = max(dist)
+        if max_len < now_max:
+            max_len = now_max
+            start = dist.index(now_max)
+        elif max_len == now_max:
+            break
 
 print(max_len)
-'''
