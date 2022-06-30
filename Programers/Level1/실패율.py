61d0b07e224f59732886abefc43e2b4222400022
TRASALBY <ghtjd8264@naver.com>
2022-06-30 18:17:55 +0900

    0630 프로그래머스 Level1 수정
    효율적인 과정으로 수정

--- Programers/Level1/실패율.py
+++ Programers/Level1/실패율.py
@@ -1,57 +1,22 @@
 def solution(N, stages):
-    answer = [ 0 for _ in range(N)] 
-    result = []                 #1/8 , 3/7, 2/4, 1/2, 0
-                                #0.125, 0.428, 0.5, 0.5, 0
-    people = len(stages)        #8
-    for i in range(1,N+1):
-        count1 = 0
-        count2 = 0
-        for stage in stages:
-            if i <= stage:
-                count1 += 1
-            if i == stage:
-                count2 += 1
-        if count1 == 0:
-            count1+=1
-        result.append(count2/count1)
-    def solution(N, stages):
     answer = []
-    result = []                 #1/8 , 3/7, 2/4, 1/2, 0
-                                #0.125, 0.428, 0.5, 0.5, 0
-    people = len(stages)        #8
-    for i in range(1,N+1):
-        count1 = 0
-        count2 = 0
-        for stage in stages:
-            if i <= stage:
-                count1 += 1
-            if i == stage:
-                count2 += 1
-        if count1 == 0:
-            count1+=1
-        result.append(count2/count1)
+    clear = []
     
-
-    for k in range(len(result)):
-        max = -1
-        for i in result:
-            if i > max:
-                max = i
-        answer.append(result.index(max) + 1)
-        result[result.index(max)] = -1
+    for i in range(1,N+1):
+        c,f = 0,0
+        for j in range(len(stages)):
+            if i == stages[j]:
+                f+=1
+            if i <= stages[j]:
+                c+=1
+        if c == 0:
+            clear.append([0,i])
+        else:
+            clear.append([f/c,i])
+        
+    clear.sort(key = lambda x : x[0],reverse = -1)
+    print(clear)
+    
+    for i in range(len(clear)):
+        answer.append(clear[i][1])
     return answer
-    rank = 1
-    for k in range(len(result)):
-        max = -1
-        for i in result:
-            if i > max:
-                max = i
-        answer[result.index(max)] = rank
-        rank += 1
-        result[result.index(max)] = -1
-    return answer
-
-stages= 	[4,4,4,4,4]
-N = 4
-
-solution(N,stages)
