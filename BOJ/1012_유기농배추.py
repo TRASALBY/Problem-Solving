df751f22d54f8f96a945cd898a15a9656bf89639
TRASALBY <ghtjd8264@naver.com>
2022-07-01 23:08:32 +0900

    0701 백준 유기농배추 재 풀이
    bfs로 접근해 시도

--- BOJ/1012_유기농배추.py
+++ BOJ/1012_유기농배추.py
@@ -1,17 +1,7 @@
-def dfs(x, y):
-    if x<=-1 or y<=-1 or x>= m or y >=n:
-        return False
-    if graph[y][x] == 1:
-            graph[y][x] = 0
-            dfs(x+1,y)
-            dfs(x-1,y)
-            dfs(x,y+1)
-            dfs(x,y-1)
-            return True
-    return False
+import sys
+from collections import deque
 
-
-
+input = sys.stdin.readline
 
 t = int(input())
 result_list=[]
@@ -20,16 +10,30 @@ for q in range(t):
 
     graph = [[0]*m for j in range(n)]
     
-    
     for _ in range(k):
         x, y = map(int,input().split())
         graph[y][x] = 1 
     result = 0
     
+    
     for i in range(n):
         for j in range(m):
-            if dfs(j,i) == True:
-                result +=1
+            if graph[i][j] == 1:
+                q = deque([(i,j)])
+                dx = [-1,1,0,0]
+                dy = [0,0,-1,1]
+
+                while(q):
+                    x,y = q.popleft()
+
+                    for k in range(4):
+                        nx = x + dx[k]
+                        ny = y + dy[k]
+
+                        if 0 <= nx <n and 0 <= ny < m and graph[nx][ny] ==1:
+                            q.append([nx,ny])
+                            graph[nx][ny] = 0
+                result+= 1
     result_list.append(result)
     
 for answer in result_list:
