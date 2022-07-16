decccd76302ec81eb868b4a2243978877b63f28e
TRASALBY <ghtjd8264@naver.com>
2022-07-16 23:36:29 +0900

    0716 백준
    가장 앞에 있는 문서의 중요도를 확인하여 중요도가 더 높은 문서가 존재한다면 가장 뒤로 이동시킨다.

---
+++ BOJ/1966_프린터큐.py
@@ -0,0 +1,21 @@
+test_case = int(input())
+ 
+for i in range(test_case):
+    n, m = map(int, input().split())
+    queue = list(map(int, input().split()))
+    cnt = 0	
+    while(m != -1):
+        if queue[0] == max(queue):
+            del queue[0]
+            m -= 1
+            cnt += 1
+        else:
+            queue.append(queue[0])
+            del queue[0]
+            
+            if m == 0:
+                m = len(queue) - 1
+            else:
+                m -= 1
+                
+    print(cnt)
